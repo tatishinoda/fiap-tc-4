ec23ad3323a237db23ffb298ef6528ea6c66b310
Fernanda Campos <fernanda.jiacinto@comexport.com.br>
2026-01-20 14:36:04 -0300

    Melhorias na estrutura

--- src/domain/usecases/auth/LogoutUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { IAuthRepository } from '../../../domain/repositories/IAuthRepository';
-
-export class LogoutUseCase {
-  constructor(private authRepository: IAuthRepository) {}
-
-  async execute(): Promise<void> {
-    await this.authRepository.logout();
-  }
-}
